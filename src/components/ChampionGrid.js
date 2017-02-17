bf963961579968cd9e7c76b4a4636bcc29603ec0
Eric <caokeric@gmail.com>
2017-02-16 19:45:41 -0800

    Make ChampionGrid filterable by name or tag.

--- src/components/ChampionGrid.js
+++ src/components/ChampionGrid.js
@@ -4,11 +4,31 @@ import ChampionSquare from './ChampionSquare';
 import './ChampionGrid.css';
 
 class ChampionGrid extends Component {
+    constructor(props) {
+        super(props);
+        this.state = ({
+            filteredChampions: []
+        });
 
-    championSquaresList = () => {
-        const { champions, onChampionClick, unavailableChampions } = this.props;
+        this.championSquaresList = this.championSquaresList.bind(this);
+        this.filterByNameOrTag = this.filterByNameOrTag.bind(this);
+        this.filterByTag = this.filterByTag.bind(this);
+    }
 
-        return champions.map(champ => {
+    componentWillReceiveProps(nextProps) {
+        // Set default filtered data to all champions
+        if (this.props.champions.length !== nextProps.champions.length) {
+            this.setState({
+                filteredChampions: nextProps.champions
+            });
+        }
+    }
+
+    championSquaresList() {
+        const { onChampionClick, unavailableChampions } = this.props;
+        const { filteredChampions } = this.state;
+
+        return filteredChampions.map(champ => {
             const unavailable = unavailableChampions.includes(champ.id) ? 'unavailable' : 'available';
             return (
                 <div key={champ.id} className={`champion-square-wrapper ${unavailable}`}>
@@ -19,17 +39,34 @@ class ChampionGrid extends Component {
                 </div>
             );
         });
-    }    
+    }
+
+    filterByNameOrTag(event) {
+        const filterRegex = new RegExp(event.target.value, 'i');
+        const filteredChampions = this.props.champions.filter(champ => {
+            return filterRegex.test(champ.id) || this.filterByTag(champ.tags, filterRegex).length > 0;
+        });
+
+        this.setState({
+            filteredChampions: filteredChampions
+        });
+    }
+
+    filterByTag(tags, filterRegex) {
+        return tags.filter(tag => {
+            return filterRegex.test(tag);
+        });
+    }
 
     render() {
-        const listChampions = this.championSquaresList();
+        const championSquaresList = this.championSquaresList();
         return (
             <div className="champion-grid">
                 <div className="filter-container">
-                    <input type="text" name="filter" />
+                    <input type="text" name="filter" onChange={this.filterByNameOrTag} />
                 </div>
                 <div className="champion-grid-items">
-                    {listChampions}
+                    {championSquaresList}
                 </div>
             </div>
         );
